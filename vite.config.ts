add50a2ed09188ec83244a0cb84c0ac417deab5a
Vincent Yono <vincentyono@protonmail.com>
2025-07-04 23:56:39 +0700

    patch: use path instead of node:path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "node:path";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
